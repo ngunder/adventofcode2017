ff0b92b97bc9a9445a462c179e8304fbf7cd5395
Nicholas Gunder <ngu@tradeshift.com>
2017-12-17 18:16:37 +0100

    Day 17 completed

--- lib/aoc2017.ex
+++ lib/aoc2017.ex
@@ -85,6 +85,10 @@ defmodule Aoc2017 do
     IO.inspect Day16.part_a, label: "Day 16 part A"
     IO.inspect Day16.part_b, label: "Day 16 part B"
   end
+  def do_day(17) do
+    IO.inspect Day17.part_a, label: "Day 17 part A"
+    IO.inspect Day17.part_b, label: "Day 17 part B"
+  end
   def do_day(invalid) do
     IO.inspect invalid, label: "This day is invalid"
   end
